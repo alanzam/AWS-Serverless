55f23de2005bda78c968243f484fe6fd92f5c0eb
Alan <alanzam@msn.com>
2018-08-14 14:19:46 -0500

    Update PPT

---
+++ aws-serverless-sample/S3/site/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_1XAUrz0B0', // e.g. us-east-1_1XAUrz0B0
+        userPoolClientId: '7pu257v063smuholh6k7o7iouv', // e.g. 7pu257v063smuholh6k7o7iouv
+        region: 'us-east-1' // e.g. us-east-1
+    },
+    api: {
+        invokeUrl: ''
+    }
+};
